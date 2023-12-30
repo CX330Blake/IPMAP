9537e3f4cbd71d87892abfb2412f2967530a48a4
DESKTOP-COR4OEI\陳子雋 <timchen975@gmail.com>
2023-12-30 21:49:47 +0800

    Add prettytable to IPMAP.py

--- IPMAP.py
+++ IPMAP.py
@@ -22,7 +22,7 @@ def install_requirement():
         print("\nError installing requirements. Please make sure 'pip' is installed.\n")
 
 
-required_modules = ["requests", "rgbprint"]
+required_modules = ["requests", "rgbprint", "prettytable"]
 if all(check_module(module) for module in required_modules):
     pass
 else:
@@ -31,6 +31,7 @@ else:
 from rgbprint import gradient_print, Color, rgbprint
 import requests
 import webbrowser
+import prettytable
 
 
 # def open_map(coordinate):
@@ -46,41 +47,34 @@ def get_ip_info():
     api_url = f"https://ipinfo.io/{target_ip}/json"
     response = requests.get(api_url)
     ip_info = response.json()
+    if ip_info.get("status") == 404:
+        rgbprint(f"\nIP {target_ip} NOT FOUND !!!", color="red")
+        input(f"\n{Color.red}Press enter to continue...{Color.red}")
+        return
     print()
-    ip_address = f"[+] IP address: {ip_info.get('ip', 'N/A')}"
-    location = f"[+] Location: {ip_info.get('city', 'N/A')}, {ip_info.get('region', 'N/A')}, {ip_info.get('country', 'N/A')}"
-    coordinate_str = f"[+] Coordinate: ({ip_info.get('loc', 'N/A')})"
     coordinate = ip_info.get("loc", "N/A")
     latitude = coordinate.split(",")[0]
     longitude = coordinate.split(",")[1]
-    ISP = f"[+] ISP: {ip_info.get('org', 'N/A')}"
-    postal = f"[+] Postal: {ip_info.get('postal', 'N/A')}"
-    google_maps_url = (
-        f"[+] Google Map: https://www.google.com/maps/search/{latitude},{longitude}"
+    google_maps_url = f"https://www.google.com/maps/search/{latitude},{longitude}"
+    data_table = prettytable.PrettyTable()
+    data_table.field_names = ["IP Address", ip_info.get("ip", "N/A")]
+    data_table.add_row(
+        [
+            "Location",
+            f"{ip_info.get('city', 'N/A')}, {ip_info.get('region', 'N/A')}, {ip_info.get('country', 'N/A')}",
+        ]
     )
-    for char in ip_address:
+    data_table.add_row(["Coordinate", ip_info.get("loc", "N/A")])
+    data_table.add_row(["ISP", ip_info.get("org", "N/A")])
+    data_table.add_row(["Postal", ip_info.get("postal", "N/A")])
+    data_table.add_row(["Google Map", google_maps_url])
+
+    # 获取表格的字符串表示
+    table_str = data_table.get_string()
+
+    for char in table_str:
         rgbprint(char, color=0x4BBEE3, end="")
-        time.sleep(0.007)
-    print()
-    for char in location:
-        rgbprint(char, color=0x4BBEE3, end="")
-        time.sleep(0.007)
-    print()
-    for char in coordinate_str:
-        rgbprint(char, color=0x4BBEE3, end="")
-        time.sleep(0.007)
-    print()
-    for char in ISP:
-        rgbprint(char, color=0x4BBEE3, end="")
-        time.sleep(0.007)
-    print()
-    for char in postal:
-        rgbprint(char, color=0x4BBEE3, end="")
-        time.sleep(0.007)
-    print()
-    for char in google_maps_url:
-        rgbprint(char, color=0x4BBEE3, end="")
-        time.sleep(0.007)
+        time.sleep(0.005)
     print()
 
 
@@ -89,43 +83,35 @@ def get_my_ip():
     api_url = f"https://ipinfo.io/json"
     response = requests.get(api_url)
     ip_info = response.json()
-    ip_address = f"[+] IP address: {ip_info.get('ip', 'N/A')}"
-    location = f"[+] Location: {ip_info.get('city', 'N/A')}, {ip_info.get('region', 'N/A')}, {ip_info.get('country', 'N/A')}"
-    coordinate_str = f"[+] Coordinate: ({ip_info.get('loc', 'N/A')})"
     coordinate = ip_info.get("loc", "N/A")
     latitude = coordinate.split(",")[0]
     longitude = coordinate.split(",")[1]
-    ISP = f"[+] ISP: {ip_info.get('org', 'N/A')}"
     google_maps_url = (
         f"[+] Google Map: https://www.google.com/maps/search/{latitude},{longitude}"
     )
-    for char in ip_address:
+    coordinate = ip_info.get("loc", "N/A")
+    latitude = coordinate.split(",")[0]
+    longitude = coordinate.split(",")[1]
+    google_maps_url = f"https://www.google.com/maps/search/{latitude},{longitude}"
+    data_table = prettytable.PrettyTable()
+    data_table.field_names = ["IP Address", ip_info.get("ip", "N/A")]
+    data_table.add_row(
+        [
+            "Location",
+            f"{ip_info.get('city', 'N/A')}, {ip_info.get('region', 'N/A')}, {ip_info.get('country', 'N/A')}",
+        ]
+    )
+    data_table.add_row(["Coordinate", ip_info.get("loc", "N/A")])
+    data_table.add_row(["ISP", ip_info.get("org", "N/A")])
+    data_table.add_row(["Google Map", google_maps_url])
+
+    # 获取表格的字符串表示
+    table_str = data_table.get_string()
+
+    for char in table_str:
         rgbprint(char, color=0x4BBEE3, end="")
-        time.sleep(0.007)
+        time.sleep(0.005)
     print()
-    for char in location:
-        rgbprint(char, color=0x4BBEE3, end="")
-        time.sleep(0.007)
-    print()
-    for char in coordinate_str:
-        rgbprint(char, color=0x4BBEE3, end="")
-        time.sleep(0.007)
-    print()
-    for char in ISP:
-        rgbprint(char, color=0x4BBEE3, end="")
-        time.sleep(0.007)
-    print()
-    for char in google_maps_url:
-        rgbprint(char, color=0x4BBEE3, end="")
-        time.sleep(0.007)
-    print()
-    # openmap = input("Open Map on browser? (Y/n)")
-    # if openmap == "":
-    #     open_map(ip_info.get("loc"))
-    # elif openmap[0].lower() == "n" or openmap[0].lower() == "no":
-    #     pass
-    # else:
-    #     open_map(ip_info.get("loc"))
 
 
 def print_start():
@@ -139,6 +125,8 @@ __/ /  _  ____/_  /  / / _  ___ |  ____/
 </> Create by CX330Blake
 </> Version 1.1
 -----------------------------------------
+
+
 """
     gradient_print(
         hello, start_color=0x4BBEE3, end_color=Color.medium_violet_red, end=""
@@ -163,7 +151,7 @@ if __name__ == "__main__":
         print_start()
         options = int(
             input(
-                f"{Color.medium_violet_red}Choose an option:\n[1] Find my IP\n[2] Find other's IP\n[3] QUIT\nChoose: {Color.medium_violet_red}"
+                f"{Color.medium_violet_red}[+] Choose an option:\n\n[1] Find my IP\n[2] Find other's IP\n[3] QUIT\n[>] Choose: {Color.medium_violet_red}"
             )
         )
         if options == 1:
